ccd9a54f0ad172405101c8d04007df44d42c529f
Greg Osha <gregory.osha@gmail.com>
2023-11-07 14:54:40 -0500

    chaned out ports

--- servo_test.py
+++ servo_test.py
@@ -13,7 +13,7 @@ from adafruit_pca9685 import PCA9685
 #Servo Juice
 from adafruit_motor import servo
 
-i2c = busio.I2C(board.SCL, board.SDA)
+i2c = busio.I2C(board.GP3, board.GP2)
 
 
 servo_arr = servo.Servo
