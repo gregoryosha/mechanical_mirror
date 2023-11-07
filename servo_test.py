65ff6a30934271e0e6f68f30504d65ce3bcfb71c
Greg Osha <gregory.osha@gmail.com>
2023-11-07 16:29:26 -0500

    changed wave form

--- servo_test.py
+++ servo_test.py
@@ -38,21 +38,21 @@ while True:
         while True:
             try:
                 for n in range(box_num):
-                    for i in range(4):
-                        for j in range(4):
+                    for j in range(4):
+                        for i in range(4):
                             servo_arr(pca_arr[n].channels[i*4 + j]).angle = out_ang
-                        time.sleep(0.05)
+                        time.sleep(0.1)
                 for n in range(box_num):
-                    for i in range(4):
-                        for j in range(4):
+                    for j in range(4):
+                        for i in range(4):
                             servo_arr(pca_arr[n].channels[i*4 + j]).angle = in_ang
-                        time.sleep(0.05)
+                        time.sleep(0.1)
             except:
                 for n in range(box_num):
-                    for i in range(4):
-                        for j in range(4):
+                    for j in range(4):
+                        for i in range(4):
                             servo_arr(pca_arr[n].channels[i*4 + j]).angle = in_ang
-                        time.sleep(0.05)
+                        time.sleep(0.1)
                 break
     else:
         for i in range(16):
