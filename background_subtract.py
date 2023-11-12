16ec949a9cc725924b48a3cf00d5f08a3d334a74
Greg Osha <gregory.osha@gmail.com>
2023-11-12 18:07:04 -0500

    test on pi

--- background_subtract.py
+++ background_subtract.py
@@ -14,18 +14,22 @@ def pixelate(img):
     return output
 
 cap = cv2.VideoCapture(0)
-#kernel = cv.getStructuringElement(cv.MORPH_ELLIPSE, (3,3))
+kernel = cv2.getStructuringElement(cv2.MORPH_ELLIPSE, (3,3))
 #fgbg = cv2.bgsegm.createBackgroundSubtractorMOG()
-#fgbg = cv.bgsegm.BackgroundSubtractorGMG()
-fgbg = cv2.createBackgroundSubtractorMOG2(detectShadows=True)
-#fgbg = cv.createBackgroundSubtractorKNN(detectShadows=True)
+#fgbg = cv2.bgsegm.BackgroundSubtractorGMG()
+#fgbg = cv2.createBackgroundSubtractorMOG2(detectShadows=True)
+fgbg = cv2.createBackgroundSubtractorKNN(detectShadows=True)
 while True:
     ret, frame = cap.read()
     if frame is None:
         break
     fgmask = fgbg.apply(frame)
-    #fgmask = cv.morphologyEx(fgmask, cv.MORPH_OPEN, kernel)
-    pixelated_image = pixelate(fgmask)
+    fgmask = cv2.morphologyEx(fgmask, cv2.MORPH_OPEN, kernel)
+
+    THRESHHOLD = 150
+    thresh, b_w_image = cv2.threshold(fgmask, THRESHHOLD, 255, cv2.THRESH_BINARY)
+    b_w_image = cv2.cvtColor(b_w_image, cv2.COLOR_GRAY2RGB)
+    pixelated_image = pixelate(b_w_image)
 
     cv2.imshow('Frame', frame)
     cv2.imshow('FG MASK Frame', fgmask)
