8b5d848a288151bbf044a4656ac6d9ff8d217ac9
Greg Osha <gregory.osha@gmail.com>
2023-11-20 11:55:59 -0500

    changed baudrate

--- serial_test/serial_write.py
+++ serial_test/serial_write.py
@@ -4,11 +4,8 @@ import serial
 
 ser = serial.Serial(
         port='/dev/ttyUSB0', #Replace ttyS0 with ttyAM0 for Pi1,Pi2,Pi0
-        baudrate = 9600,
-        parity=serial.PARITY_NONE,
-        stopbits=serial.STOPBITS_ONE,
-        bytesize=serial.EIGHTBITS,
-        timeout=1
+        baudrate = 115200,
+        timeout=0.1
 )
 def write_read(x): 
         ser.write(bytes(x, 'utf-8')) 
