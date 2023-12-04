8ce9bc29885a2ba8087c08fffa41c71f24f9faf8
Greg Osha <gregory.osha@gmail.com>
2023-12-04 14:04:59 -0500

    crashing issue

--- servo_test/servo_test.py
+++ servo_test/servo_test.py
@@ -13,7 +13,7 @@ from adafruit_pca9685 import PCA9685
 #Servo Juice
 from adafruit_motor import servo
 
-# i2c = busio.I2C(board.SCL, board.SDA)
+#i2c = busio.I2C(board.SCL, board.SDA)
 i2c = busio.I2C(board.SCL1, board.SDA1)
 
 
