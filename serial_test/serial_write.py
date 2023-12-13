6e68a867f5bb904ca46156d0e978d1fe160a81cb
Greg Osha <gregory.osha@gmail.com>
2023-12-13 13:01:38 -0500

    serial debugging

--- serial_test/serial_write.py
+++ serial_test/serial_write.py
@@ -3,16 +3,16 @@ import time
 import serial
 
 ser = serial.Serial(
-        port='/dev/ttyUSB0', #Replace ttyS0 with ttyAM0 for Pi1,Pi2,Pi0
+        port='/dev/ttyACM0', #Replace ttyS0 with ttyAM0 for Pi1,Pi2,Pi0
         baudrate = 115200,
         timeout=0.1
 )
-def write_read(x): 
-        ser.write(bytes(x, 'utf-8')) 
-        line = ser.readline() 
-        line = line.decode("utf-8","ignore")
-        return line
+        
 while True: 
         num = input("Enter a number: ") # Taking input from user 
-        value = write_read(num) 
-        print(value) # printing the value 
+        ser.write(bytes(num, 'utf-8')) 
+        if ser.in_waiting:
+                line = ser.readline()
+                line = line.decode("utf-8","ignore")
+                print(line)
+        
