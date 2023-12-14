4c929533bc8f027582d06059db5b14ea70148d94
Greg Osha <gregory.osha@gmail.com>
2023-12-14 15:39:40 -0500

    new port

--- serial_test/serial_bitpack.py
+++ serial_test/serial_bitpack.py
@@ -74,7 +74,7 @@ def encodeStates(states: list[int]) -> bytes:
 
 def main():
     ser = serial.Serial(
-        port='/dev/ttyACM2', #Replace ttyS0 with ttyAM0 for Pi1,Pi2,Pi0
+        port='/dev/ttyUSB0', #Replace ttyS0 with ttyAM0 for Pi1,Pi2,Pi0
         baudrate = 115200,
         timeout=1
     )
