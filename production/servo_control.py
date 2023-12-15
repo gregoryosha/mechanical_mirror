0bd0c347833afdce0d1ce5ed81eb4f41fd2975af
Greg Osha <gregory.osha@gmail.com>
2023-12-15 13:22:31 -0500

    took out delay

--- production/servo_control.py
+++ production/servo_control.py
@@ -77,7 +77,7 @@ def main():
                 for j in range(4):
                     for i in range(4):
                         servo_arr(pca_arr[n].channels[i*4 + 3-j]).angle = IN_ANG
-                    time.sleep(0.1)
+                
 
 if __name__ == '__main__':
     main()
