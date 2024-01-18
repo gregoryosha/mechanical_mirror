d90f259557dae85cef531594b3c3adafca1a7c9c
Greg Osha <gregory.osha@gmail.com>
2024-01-18 13:00:53 -0500

    upped reset delay

--- production/servo_control.py
+++ production/servo_control.py
@@ -65,13 +65,13 @@ def reload(servo_arr, pca_arr):
         for j in range(4):
             for i in range(4):
                 servo_arr(pca_arr[n].channels[i*4 + 3-j]).angle = IN_ANG
-            time.sleep(0.01)
+            time.sleep(0.02)
     
     for n in range(BOX_NUM):
         for j in range(4):
             for i in range(4):
                 servo_arr(pca_arr[n].channels[i*4 + 3-j]).angle = None
-            time.sleep(0.01)
+            time.sleep(0.02)
 
 
 
